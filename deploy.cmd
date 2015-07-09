54334e36681b278a02a386c301d8ba0566696dea
rdvcordeiro <rdvcordeiro@gmail.com>
2015-07-09 22:56:41 +0100

    Initial commit

--- deploy.cmd
+++ deploy.cmd
@@ -87,16 +87,16 @@ goto :EOF
 
 :Deployment
 echo Handling node.js deployment.
-
+echo 1
 :: 1. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
-
+echo 2
 :: 2. Select node version
 call :SelectNodeVersion
-
+echo 3
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
@@ -104,6 +104,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
+echo 4
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
